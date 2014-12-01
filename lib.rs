4bf83bd652b0ae006ef60c18a2c759a8674ab07e
Mikko Perttunen <cyndis@kapsi.fi>
2014-12-01 13:14:23 +0200

    Add encoding support

--- lib.rs
+++ lib.rs
@@ -8,8 +8,19 @@ pub fn decode_codepoint(codepoint: u16) -> Option<char> {
     map::decode(codepoint)
 }
 
+/// Encode a single Unicode codepoint. Returns None if no mapping exists.
+pub fn encode_codepoint(codepoint: char) -> Option<u16> {
+    map::encode(codepoint)
+}
+
 #[test]
 fn test_decode() {
     assert_eq!(decode_codepoint(0x2341), std::char::from_u32(0xff21));
     assert_eq!(decode_codepoint(0x3000), None);
+}
+
+#[test]
+fn test_encode() {
+    assert_eq!(encode_codepoint(std::char::from_u32(0xff21).unwrap()), Some(0x2341));
+    assert_eq!(encode_codepoint('a'), None);
 }
